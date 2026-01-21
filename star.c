6beaeaeccac70b798f5cbfb35c661598d4fb96d9
Hello-pythworld <yeonghun4051@daum.net>
2026-01-21 10:11:27 +0900

    아마 충돌

---
+++ star.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    for(int j=2; j<=9; j++){
+            printf("%d단\n", j);
+    for (int i = 1; i <=9; i++) {
+        printf("%d * %d = %d\n", j, i, j*i);
+    }
+
+}
+    return 0;
+}
