54786733205e4a7b9f132848bf0c154cdc31ecf3
Hello-pythworld <yeonghun4051@daum.net>
2026-03-06 18:17:06 +0900

    2026-03-06

---
+++ 5. HTML + CSS + Javascript/4.react-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import NewVariable from './components/01_NewVariable';
+
+
+
+function App() {
+
+  return (
+    <NewVariable />
+  )
+}
+export default App
---
+++ 5. HTML + CSS + Javascript/4.react-project/src/pages/05_Extension.jsx
@@ -0,0 +1,19 @@
+import { useParams } from "react-router-dom"
+
+export default function Extension() {
+    const todoHeader = "todo-header"
+
+    return(
+        <>
+            <h1 className={todoHeader}>Todo List</h1>
+        
+        <ul style = {{backGroundColor : 'red', color: 'blue'}}>
+        <li>잠자기</li>
+        <li>먹기</li>
+        <li>놀기</li>        
+        </ul>
+    
+        </>
+    )
+
+}
